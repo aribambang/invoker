68c2a151a0c3004a147588562a693ad44ea2960d
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-08-09 03:53:10 +0700

    add routes

--- web/app.py
+++ web/app.py
@@ -142,3 +142,10 @@ class Refill(Resource):
     })
 
     return jsonify( generateReturnDictionary(200, "Refilled successfully"))
+
+api.add_resource(Register, "/register")
+api.add_resource(Classify, "/classify")
+api.add_resource(Refill, "/refill")
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0")
